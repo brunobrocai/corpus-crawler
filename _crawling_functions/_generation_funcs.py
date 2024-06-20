bed108788fbfffc264ee7d163ff8a3da88a58acb
Bruno Brocai <bruno-brocai@gmx.de>
2024-06-20 09:50:15 +0200

    Add Zeit url generator

--- _crawling_functions/_generation_funcs.py
+++ _crawling_functions/_generation_funcs.py
@@ -11,9 +11,9 @@ def generate_spektrum_links(pages, dir_path, base_url):
     )
 
 
-# def generate_zeit_links(baselink, pages, queuepath):
-#     link_set = set()
-#     link_set.add(baselink)
-#     for i in range(2, pages+1):
-#         link_set.add(f'{baselink}&p={str(i)}')
-#     write_data.write_lines_to_file(queuepath, link_set)
+def generate_zeit_links(pages, dirpath, base_url):
+    link_set = set()
+    link_set.add(base_url)
+    for i in range(2, pages+1):
+        link_set.add(f'{base_url}&p={str(i)}')
+    write_data.write_lines_to_file(dirpath, link_set)
